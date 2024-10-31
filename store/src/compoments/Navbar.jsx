d1a5bbd4c7c35254f8a060381a29457507d3979d
Pedro Simão <pedrosimaocontato@gmail.com>
2024-10-30 22:18:53 -0300

    feita a estilização dos links

--- store/src/compoments/Navbar.jsx
+++ store/src/compoments/Navbar.jsx
@@ -4,10 +4,11 @@ import '/node_modules/primeflex/primeflex.css'
 
 const Navbar = () => {
     return (
-        <nav > 
-            <Link to="/produtos" style={{textDecoration: "none"}}>Produtos</Link>
-            <Link to="/categorias" style={{textDecoration: "none"}}>Categorias</Link>
-            <Link to="/pedidos" style={{textDecoration: "none"}}>Meus Pedidos</Link>
+        <nav className="pl-8 pt-6 text-2xl"> 
+            <Link to="/"style={{textDecoration: "none", color: "#474747"}}>Home</Link>
+            <Link to="/produtos" style={{textDecoration: "none", color: "#474747"}} className="pl-5">Produtos</Link>
+            <Link to="/categorias" style={{textDecoration: "none", color: "#474747"}} className="pl-5">Categorias</Link>
+            <Link to="/pedidos" style={{textDecoration: "none", color: "#474747"}} className="pl-5">Meus Pedidos</Link>
         </nav>
     );
 };
